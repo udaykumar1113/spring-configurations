56db6e958386b28a1767813fcee41a4016762338
uday <udaykumar1113@gmail.com>
2017-11-09 22:11:00 -0500

    created class for configurations

---
+++ src/main/java/com/jokes/SpringConfigurationsApplication.java
@@ -0,0 +1,12 @@
+package com.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigurationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigurationsApplication.class, args);
+	}
+}
