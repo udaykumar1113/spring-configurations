ede3edd2270de2ea4945b4466a3a7001dd748ff5
uday <udaykumar1113@gmail.com>
2017-11-12 15:57:49 -0500

    added XML configuration

--- src/main/java/com/jokes/SpringConfigurationsApplication.java
+++ src/main/java/com/jokes/SpringConfigurationsApplication.java
@@ -2,8 +2,10 @@ package com.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringConfigurationsApplication {
 
 	public static void main(String[] args) {
